afa275d82432ed9e6afa9c89cfa0d2d08d0c0748
Park Sang-Yeop <112646715+2201204@users.noreply.github.com>
2022-11-12 23:07:05 +0900

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -2,11 +2,13 @@
 import turtle
 t=turtle.Turtle()
 t.shape('turtle')
-t.left(90)
-for i in range(6):
+t.left(90) #왼쪽으로 90도 회전함
+for i in range(6): #밑의 과정을 6번 반복함
     t.right(60)
-    t.forward(100); t.forward(-30); t.left(60); t.forward(30); t.forward(-30);
-    t.right(120); t.forward(30); t.forward(-30); t.left(60); t.forward(-70)
+    t.forward(100); t.forward(-30); t.left(60); t.forward(30); t.forward(-30);  #앞으로 100만큼 이동하고 뒤로 30만큼 이동하고
+                                                                                #왼쪽으로 60도 회전하여 앞으로 30만큼 이동하고 30만큼 뒤로 이동한다.
+    t.right(120); t.forward(30); t.forward(-30); t.left(60); t.forward(-70)     #오른쪽으로 120도 회전하여 앞으로 30만큼 이동하고, 30만큼 뒤로 이동한다.
+                                                                                #왼쪽으로 60도 회전하여 #중앙으로 되돌아온다.
     
     8.
     import turtle
